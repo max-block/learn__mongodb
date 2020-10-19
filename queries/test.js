cc7d83a2b61a490b1cb70e0f406b2d5fd97ba109
Max Block <maxblock@pm.me>
2020-10-19 10:24:55 +0300

    update

--- queries/test.js
+++ queries/test.js
@@ -1,8 +1,9 @@
-db.test.drop()
+db.test.drop();
 db.test.insertMany([
-    { name: "n1", a: 10, b: 20 },
-    { name: "n2", a: 30, b: 400 },
-])
-
-
-db.test.find({"$expr": {"$gt": ["$a", {"$subtract": ["$b", 20]}]}})
+    {name: "n1", created_at: ISODate("2020-12-11T14:12:00Z")},
+    {name: "n2", created_at: null},
+    {name: "n3", created_at: ISODate("2014-12-11T14:12:00Z")},
+    {name: "n4", created_at: null},
+    {name: "n6", created_at: ISODate("2016-12-11T14:12:00Z")}
+]);
+db.test.find({"$or": [{"created_at": null}, {"created_at": {"$lt": ISODate("2022-12-11T14:12:00Z")}}]}).sort({"checked_at": 1});
